0c6fdfdf55a72ce7557c1330f04d77d1c367e942
khamruddin <74547308+khamruddin@users.noreply.github.com>
2023-01-12 05:49:29 +0000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -27,7 +27,7 @@ pipeline {
                   sh '''
                 
               #!/bin/bash
-              flutter build apk --no-sound-null-safety
+              //flutter build apk --no-sound-null-safety
               flutter build apk --debug --ignore-deprecation
               //flutter build apk --no-sound-null-safety
               '''
